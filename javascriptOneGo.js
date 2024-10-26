93166e60876dfefd925c843d0c06f90e275c3deb
2018amandeep <amandeep.p.2015@gmail.com>
2024-10-26 10:42:48 +0530

    functions

---
+++ javascriptOneGo.js
@@ -0,0 +1,18 @@
+// Functions
+
+// Function declaration
+
+function greet(name) {
+    return `Hello ${name}`
+}
+
+// Function invocation
+greet('Aman');
+
+// Function Expression
+
+let functionN = function(name){  // address of function has been passed to the variable functionN
+    return `Hello ${name}`
+}
+
+console.lof(functionN)
