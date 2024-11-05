1e8b44e6be3a601b6d4165c228a9bc64f660fa7d
2018amandeep <amandeep.p.2015@gmail.com>
2024-11-05 19:20:22 +0530

    sql 3:29 sub query

--- select.sql
+++ select.sql
@@ -99,3 +99,82 @@ select * from table1 order by 2,1 -- 2,1 denotes here the columns 1-> column1, 2
 
 -- Case clause -> Allows to apply logics into table
 
+select name, level, level >= 20 as minimum_level -- it will return as true or false
+from characters
+
+--upto 15 -low exp
+-- 15- 25 - med exp
+-- 25-40 - high exp
+
+select name,
+case 
+when level <= 15 then 'low exp'
+when level >15 and level <=25 then 'medium exp'
+when level > 25 'super exp'
+end as level_bucket
+from characters
+
+
+to optimize it we can remove level>15 in second condition because it will only come to second statement when it will fail on first statement
+
+so we can write it as 
+
+case 
+when level< 15 then ' low'
+when level <25 then 'mid'
+else 'high'  -- if we remove this statement then it will return null here
+end as level_bucket
+
+
+-- AGGREGATIONS -- 
+
+1. SUM
+2. MIN
+3. MAX
+4. AVG
+5. COUNT
+6. COUNT/_NUMBER
+
+select sum(level) as sum, min(level), max(level), avg(level), count(level) from charachters. 
+
+-- agg on strings
+the way strings are sorted in sql are called lexical graphic order basically a fance word for alphabetical order
+Min(String) it will fetch the first string value where Max(string) will return the last string value
+
+STRING_AGG(STRING, ",") --  it will take all the characters and group them with , 
+-- agg functions are not allowed on where clause
+
+-- SUBQUERY --
+-- find characters whose experience is between min and max
+-- more experience than the least experience character , less than the most experience character
+
+    select name,class from characters
+    where experience > (
+        select min(experience) from characters
+    )
+    AND experience < (
+        select max(experience) from characters 
+    )
+
+-- find the difference between a characters's experience and their mentors
+
+-- uncorrelated subqueries
+select id as mantee_id, mentor_id, experience as mentee_experience,
+(
+    select experience from characters where id = mantee_table.mentor_id
+)
+as mentor_exp
+from characters as mantee_table
+where mentor_id is not null
+
+-- using cloumn for equations
+
+-- correlated subquery
+-- These queries are slow because they run saperately on each row
+select id as mantee_id, mentor_id,
+(
+    select experience from characters where id = mantee_id.mentor_id
+) - experience as experience_differencr
+from characters as mantee_id
+where id is not null
+
