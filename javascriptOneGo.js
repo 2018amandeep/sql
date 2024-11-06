9e6f0efadb00026be5b8233c6fb21ace7673bad3
2018amandeep <amandeep.p.2015@gmail.com>
2024-11-06 22:49:20 +0530

    question leetcode

--- javascriptOneGo.js
+++ javascriptOneGo.js
@@ -77,3 +77,33 @@ for(let key in student){
   // These methods to not modify original string they creates a new modified string because in javascript strings are immutable
 
   strudent.slice(start, end) // ending values are non-inclusive means it returns value till end -1
+
+  function canSortArray(nums) {
+    let preMx = 0;
+    const n = nums.length;
+    for (let i = 0; i < n; ) {
+        const cnt = bitCount(nums[i]);
+        let j = i + 1;
+        let [mi, mx] = [nums[i], nums[i]];
+        while (j < n && bitCount(nums[j]) === cnt) {
+            mi = Math.min(mi, nums[j]);
+            mx = Math.max(mx, nums[j]);
+            j++;
+        }
+        if (preMx > mi) {
+            return false;
+        }
+        preMx = mx;
+        i = j;
+    }
+    return true;
+}
+
+function bitCount(i) {
+    i = i - ((i >>> 1) & 0x55555555);
+    i = (i & 0x33333333) + ((i >>> 2) & 0x33333333);
+    i = (i + (i >>> 4)) & 0x0f0f0f0f;
+    i = i + (i >>> 8);
+    i = i + (i >>> 16);
+    return i & 0x3f;
+}
