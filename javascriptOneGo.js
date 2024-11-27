8ba26ea670e8723f81f19d859c475f10b9b21edd
2018amandeep <amandeep.p.2015@gmail.com>
2024-11-27 19:31:38 +0530

    generator function

--- javascriptOneGo.js
+++ javascriptOneGo.js
@@ -169,4 +169,38 @@ arr.slice(0,3) // 1,2  return slice of array do not change original array it
 let sliceArr = [1,2,3,4,5,6,7,8,9]
 
 sliceArr.splice(2,2,a,b)  // it will mutate the original array as [1,2,a,b,5,6,7,8,9] and it will retun [3,4] i.e.,removed array
-sliceArr.splice(2,3) // mutated in original array [1,2,6,7,8,9], output - 3,4,5
+sliceArr.splice(2,3) // mutated in original array [1,2,6,7,8,9], output - 3,4,5\
+
+
+// Q. Write a generator function that returns a generator object which yields the fibonacci sequence.
+
+// The fibonacci sequence is defined by the relation Xn = Xn-1 + Xn-2.
+
+// The first few numbers of the series are 0, 1, 1, 2, 3, 5, 8, 13.
+// Generator function
+var fibGenerator = function*() {
+    let n1 = 0, n2 = 1;
+    while(true){
+        yield n1;
+        let temp = n2;
+        n2 = n1+n2;
+        n1= temp;
+    } 
+};
+
+// Can also be done as 
+var fibGenerator = function*() {
+    let n1 = 0, n2 = 1;
+
+    while(true){
+        yield n1;
+        // let temp = n2;
+        // n2 = n1+n2;
+        // n1= temp;
+        [n1, n2] = [n2, n1 +n2]
+    }
+    
+};
+
+
+
