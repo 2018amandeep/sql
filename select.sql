a7586ad89e95eaff67d786884d7734eaaf1e0f4b
2018amandeep <amandeep.p.2015@gmail.com>
2024-11-28 22:40:15 +0530

    sub query

--- select.sql
+++ select.sql
@@ -287,3 +287,42 @@ select accepter_id as id from RequestAccepted) as nums
 group by id
 order by num desc
 limit 1
+
+
+Write a solution to report the sum of all total investment values in 2016 tiv_2016, for all policyholders who:
+
+have the same tiv_2015 value as one or more other policyholders, and
+are not located in the same city as any other policyholder (i.e., the (lat, lon) attribute pairs must be unique).
+Round tiv_2016 to two decimal places.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Insurance table:
++-----+----------+----------+-----+-----+
+| pid | tiv_2015 | tiv_2016 | lat | lon |
++-----+----------+----------+-----+-----+
+| 1   | 10       | 5        | 10  | 10  |
+| 2   | 20       | 20       | 20  | 20  |
+| 3   | 10       | 30       | 20  | 20  |
+| 4   | 10       | 40       | 40  | 40  |
++-----+----------+----------+-----+-----+
+
+select ROUND(SUM(tiv_2016),2) as tiv_2016
+from Insurance
+where tiv_2015 IN(
+    select tiv_2015
+    from Insurance
+    group by tiv_2015 
+    having count(*) > 1
+)
+and (lat, lon) IN(
+    select lat, lon from Insurance
+    group by lat, lon
+    having COUNT(*) =1
+)
+-- 45.00
