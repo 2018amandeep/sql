e67a8a4c022ea18fbdc4276d2d26e9d599ee3bc3
2018amandeep <amandeep.p.2015@gmail.com>
2024-11-28 22:09:14 +0530

    sub query

--- select.sql
+++ select.sql
@@ -279,3 +279,11 @@ GROUP BY title
 ORDER BY AVG(rating) desc, title
 limit 1)
 
+
+select id, COUNT(*) as num from
+(select requester_id as id from RequestAccepted
+UNION ALL
+select accepter_id as id from RequestAccepted) as nums
+group by id
+order by num desc
+limit 1
